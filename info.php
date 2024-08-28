e213d96d4bc439650af7b54c33c60dee0b8e4d0b
matheuscerquei <103291017+matheuscerquei@users.noreply.github.com>
2024-08-28 19:00:58 -0300

    Initial commit

---
+++ info.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php info</title>
+</head>
+<body>
+    <?php
+    $nome="zé";
+    $altura =1.89;
+    $idade =23;
+   
+    echo" $nome tem de altura $altura e a idade $idade";
+    
+    ?>
+
+</body>
+</html>
